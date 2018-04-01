24a8151e673c3e3de3ef16ba8e791c4fb94cde09
Roopam Shukla <roopamscshukla@gmail.com>
2018-04-01 19:32:56 +0530

    raw model

--- model/matchinfo.js
+++ model/matchinfo.js
@@ -1,49 +1,20 @@
 mongoose = require('mongoose');
 var room = mongoose.Schema({
-    id: {
-        type: String,
-    },
-    caller: {
-        id: {
-            type: String,
-        },
-    },
-    connections: [{
-        id: {
-            type: String,
-           
-        },
-        sdpOffer: [{
-            id: {
-                type: String,
-                
-            },
-            sdp: {}
-        }],
-        sdpAnswer: [{
-            id: {
-                type: String,
-                
-            },
-            sdp: {}
-        }],
-        iceCandidates: [{
-            candidate: {
-                type: String,
-                
-            },
-            sdpMid: {
-                type: String
-            },
-            sdpMLineIndex: {
-                type: Number
-            }
-        }]
-    }],
-    datecreated: {
-        type: Date,
-        default: Date.now
-    }
-
-})
+            "match_name": "",
+            "venue": "",
+            "date": "",
+            "time": "",
+            "match_result": "",
+            "scorecard": {
+                "First Inning": {
+                    "team": "",
+                    "batsmen": [],
+                    "bowler": []
+                },
+                "Second Inning": {
+                    "team": "",
+                    "batsmen": [],
+                    "bowler": []
+                }
+            })
 module.exports = mongoose.model('room', room);
